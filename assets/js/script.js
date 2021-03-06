698167754e7038beeddcb2fff32c919718014bf3
samuel mcswain <samuelmcswain@hotmail.com>
2021-03-06 11:15:37 -0500

    moment.js added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 
+var dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").text(dateTime);
 
 
 
 
 
 
-
-
-function getApi() {
-    var requestUrl = "api.openweathermap.org/data/2.5/weather?q={charlotte}&appid={API key}";
+function getApi(inputVal) {
+    var API_Key = "54e1375c02bb77b1a951b0a2a1264c0d"
+    var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${API_Key}&units=imperial`;
 
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
+        }).then(function(data) {
+            console.log(data);
         })
 }
+
+getApi("Paris")
